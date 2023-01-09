97bc9cdbe4caff6a4e85fdb3b015b271982b8ea0
rohitsinghind <rohitsinghind22@mail.com>
2023-01-09 20:47:22 +0530

    code push

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
+import axios from "axios";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [news, setNews] = useState([]);
+
+  const getNews = async () => {
+    axios.get("https://newsapi.org/v2/everything?q=tesla&from=2022-12-09&sortBy=publishedAt&apiKey=479a5350a3e044c6a7a21e03fd1298c8").then((res) => setNews(res.data.articles));
+  };
+
+  console.log(news)
+
+  useEffect(() => {
+    getNews()
+  }, [])
+  
+
+  return <>
+    {
+      news.map((e)=>{
+        return (
+        <>
+        <h2>{e.title}</h2>
+        <img src={e.urlToImage} alt="" />
+        <p>{e.description}</p>
+        <a href={e.url}>read more</a>
+        </>)
+      })
+    }
+  </>;
 }
 
 export default App;
